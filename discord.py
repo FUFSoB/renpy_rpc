cc39a43de7fe4de24e8254bb25630bd9a6a00f9f
FUFSoB <32937947+FUFSoB@users.noreply.github.com>
2018-09-25 20:55:10 +0500

    Add files via upload

---
+++ discord.py
@@ -0,0 +1,47 @@
+from pypresence import Presence
+import time
+import random
+import sys
+import os
+
+client_id = 'id' # id of your application
+large_text = "Game" # text of large img of your app
+large_image = "logo" # large img of your app
+small_text = "FUFSoB" # text of small img of your app
+small_image = "fuf" # small img of your app
+RPC = Presence(client_id)
+RPC.connect() # Connect RPC client
+
+while True:
+    try:
+        file = open("{0}\..\state.txt".format(os.getcwd()), 'r', encoding = "utf-8").read() # get status from file
+        
+        if "err3" == mb:
+            details = "'*.save': Status error"
+            state = "Error 3: loaded wrong save-file"
+            large_text = "Game"
+            large_image = "logo" 
+            small_text = "FUFSoB"
+            small_image = "fuf" 
+        elif "mm" == mb:
+            details = "Main Menu"
+            state = None
+            large_text = "Game"
+            large_image = "logo" 
+            small_text = "FUFSoB"
+            small_image = "fuf" 
+        elif "sp" == mb:
+            details = "Game is loading"
+            state = None
+            large_text = "Game"
+            large_image = "logo" 
+            small_text = "FUFSoB"
+            small_image = "fuf" 
+    except:
+        details = "'state.txt': Status error"
+        state = "Error 2: file is missing"
+        large_text = "Game"
+        large_image = "logo" 
+        small_text = "FUFSoB"
+        small_image = "fuf" 
+    RPC.update(details=details, state=state, large_image=large_image, large_text=large_text, small_image=small_image, small_text=small_text) # update status of rpc
---
+++ setupRPC.rpy
@@ -0,0 +1,38 @@
+init python:
+    import os
+    import subprocess
+    import io
+    discordrun = False
+    if renpy.windows:
+        try:
+            process_list = subprocess.check_output("wmic process get Description", shell=True).lower().replace("\r", "").replace(" ", "").split("\n") # get processes
+        except:
+            pass
+        try:
+            discord_list = ['discord.exe']
+            if list(set(process_list).intersection(discord_list)): # check if Discord.exe is in "process_list"
+                discordrun = True # if successfully - turn discord_rpc on
+                try: # unzip python3.6 to "game" folder, if "python.zip" exists
+                    import zipfile
+                    with zipfile.ZipFile('game/python.zip', "r") as z:
+                        z.extractall("game/")
+                    os.remove('game/python.zip') # delete "python.zip" for next getting "except"
+                except:
+                    pass
+                state = "sp" # set "loading" rpc status
+                io.open("{0}\game\state.txt".format(os.getcwd()), 'w+', encoding = "utf-8").write(state) # write "state" to file
+                p = subprocess.Popen('cd {0}\game\PPython && python "{0}\game\discord.py"'.format(os.getcwd()), shell=True) ### START RPC IN BACKGROUND ###
+        except:
+            pass
+    whathere = None
+    texthere = None
+    textthere = None
+    day = ""
+    st = ""
+
+label rpc: # callable label to set state. 
+    if discordrun:
+        python:
+            import io
+            io.open("{0}\game\state.txt".format(os.getcwd()), 'w+', encoding = "utf-8").write(state)
+    return
